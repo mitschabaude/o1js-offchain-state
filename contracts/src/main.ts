b87874ec6df3aa22e55141cda58ab40a60f2f6b7
Gregor <gregor.mitscha-baude@gmx.at>
2023-10-23 08:32:30 +0200

    more timings

--- contracts/src/main.ts
+++ contracts/src/main.ts
@@ -2,7 +2,7 @@ import { AccountUpdate, Mina, PrivateKey, Provable } from 'o1js';
 import { Key, OffchainState, Value } from './Offchain.js';
 import { compileProver } from './action-state-prover.js';
 
-const proofsEnabled = false;
+const proofsEnabled = true;
 
 let Local = Mina.LocalBlockchain({ proofsEnabled });
 Mina.setActiveInstance(Local);
@@ -24,12 +24,14 @@ if (proofsEnabled) {
 let tx;
 
 console.log('deploying...');
+console.time('deploy');
 tx = await Mina.transaction(sender, () => {
   // TODO this API is awkward. why is it under `AccountUpdate`?
   AccountUpdate.fundNewAccount(sender);
   offchain.deploy();
 });
 await tx.prove();
+console.timeEnd('deploy');
 console.log(tx.toPretty());
 await tx.sign([senderKey, zkappKey]).send();
 
@@ -38,28 +40,34 @@ let keys = [];
 // set some data
 for (let i = 0; i < 7; i++) {
   console.log('set', i, '...');
+  console.time('set');
   let key = Key.random();
   keys.push(key);
   let value = Value.random();
 
   tx = await offchain.set(sender, key, value);
+  console.timeEnd('set');
   console.log(tx.toPretty());
   await tx.sign([senderKey]).send();
 }
 
 // settle batch
 console.log('settle');
+console.time('settle');
 tx = await offchain.settleBatch(sender);
+console.timeEnd('settle');
 console.log(tx.toPretty());
 await tx.sign([senderKey]).send();
 
 // get data
 console.log('get');
+console.time('get');
 let key = keys[3];
 tx = await Mina.transaction(sender, () => {
   let value = offchain.get(key);
   // do something with value
 });
 await tx.prove();
+console.timeEnd('get');
 console.log(tx.toPretty());
 await tx.sign([senderKey]).send();

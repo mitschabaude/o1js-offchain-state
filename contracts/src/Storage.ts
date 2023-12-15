a9a871af86186e0753dae14bbc0422a667467238
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-15 14:08:10 +0100

    require caller in storage methods

--- contracts/src/Storage.ts
+++ contracts/src/Storage.ts
@@ -1,15 +1,45 @@
-import { PublicKey, SmartContract, method } from "o1js";
+import {
+  AccountUpdate,
+  Field,
+  Mina,
+  PublicKey,
+  SmartContract,
+  TokenId,
+  method,
+} from "o1js";
+
+export { Storage };
+
+class TODO extends Field {}
 
 class Storage extends SmartContract {
-  @method register(address: PublicKey) {
-    throw Error("Method not implemented.");
+  register(address: PublicKey) {
+    let caller = AccountUpdate.createSigned(address);
+    this.registerProvable(address);
+    caller.approve(this.self);
+    caller.balance.subInPlace(Mina.accountCreationFee());
+  }
+  @method registerProvable(address: PublicKey) {
+    requireSignature(address);
+    requireCaller(address, this);
   }
 
-  @method set(address: PublicKey, key: string, value: string) {
-    throw Error("Method not implemented.");
+  @method set(address: PublicKey, key: TODO, value: TODO) {
+    requireCaller(address, this);
   }
 
-  @method get(address: PublicKey, key: string) {
-    throw Error("Method not implemented.");
+  @method get(address: PublicKey, key: TODO) {
+    requireCaller(address, this);
   }
+}
+
+function requireSignature(address: PublicKey) {
+  AccountUpdate.createSigned(address);
+}
+
+function requireCaller(address: PublicKey, contract: SmartContract) {
+  contract.self.body.mayUseToken = AccountUpdate.MayUseToken.ParentsOwnToken;
+  let update = AccountUpdate.create(contract.address, TokenId.derive(address));
+  update.body.mayUseToken = AccountUpdate.MayUseToken.InheritFromParent;
+  return update;
 }
